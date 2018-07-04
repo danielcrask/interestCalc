4b5cab302d8a64cac37a67dd16194185e6725dbc
Daniel Crask <danielcrask@gmail.com>
2018-07-04 16:40:41 -0700

    Improved the red border action

--- js/main.js
+++ js/main.js
@@ -5,14 +5,15 @@ function calculateInterest() {
    var paidInterest = 0;
 
   if(interest === 0 || totalAmt === 0 || payments === 0) {
-    $('.input').css("border", "2px solid red");
+    $('.input').addClass("redBorder");
   } else {
     while(totalAmt > payments){
-      paidInterest += totalAmt * interest;
       totalAmt = totalAmt - payments;
+      paidInterest += totalAmt * interest;
 
       console.log(interest + ", " + totalAmt + ", " + payments + ", " + paidInterest);
     }
+    $('.input').removeClass("redBorder");
     paidInterest += totalAmt * interest;
     $('.interestOutput').text("Your total interest paid will be $" + paidInterest.toFixed(2));
   }
@@ -22,4 +23,5 @@ function resetFields() {
   document.getElementById('interestRate').value = "";
   document.getElementById('totalAmount').value = "";
   document.getElementById('paymentAmount').value = "";
+  $('.input').removeClass("redBorder");
 }
